1952710fcd583a67e07762980ddd0c267a42358d
Rajasri Kuchana <rajasrikuchana858@gmail.com>
2025-07-14 12:56:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+// Toggle mobile menu
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.getElementById("menu-icon");
+  const nav = document.querySelector("nav");
+
+  menuIcon.addEventListener("click", () => {
+    nav.classList.toggle("hidden");
+  });
+});
